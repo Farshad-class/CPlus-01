193ce848690548e001235b2674aa223ccad7726d
Farshad <Farshad@FARSHAD-SURFACE>
2022-02-27 14:54:19 +0330

    Add project files.

---
+++ CPlus-01/CPlus-01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "X" << "\t";
+	for (int k = 0;k <= 10;k++)
+	{
+		cout << k << "\t";
+	}
+	cout << endl;
+	for (int i = 0; i <= 10; i++)
+	{
+		cout << i << "\t";
+		for (int j = 0;j <= 10;j++)
+		{
+			cout << i * j << "\t";
+		}
+
+		cout << endl;
+	}
+}
